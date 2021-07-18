8b5bbae80a4e179ffd6139dd84e6017b5f9616d8
ok9xnirab <istiaqanirab202@gmail.com>
2021-07-18 12:37:53 +0600

    fix some sanitization and escaping issues

--- includes/Admin/views/booking-meta-form.php
+++ includes/Admin/views/booking-meta-form.php
@@ -1,7 +1,7 @@
 <table class="form-table sdevs-form">
     <tbody>
         <tr>
-            <th class="sdevs_th" scope="row"><label for="bookable_order_date">Product</label></th>
+            <th class="sdevs_th" scope="row"><label for="bookable_order_date"><?php echo esc_html__("Product", "sdevs_booking"); ?></label></th>
             <td>
                 <p class="description" id="tagline-description">
                     <a href="<?php the_permalink($post_meta["product_id"]); ?>" target="_blank">
@@ -9,30 +9,29 @@
                     </a>
                     <br />
                     <?php foreach ($attributes as $key => $value) : ?>
-                        <strong><?php echo $key; ?> : </strong> <?php echo $value; ?><br />
+                        <strong><?php echo esc_html($key); ?> : </strong> <?php echo esc_html($value); ?><br />
                     <?php endforeach; ?>
                 </p>
             </td>
         </tr>
         <tr>
-            <th class="sdevs_th" scope="row"><label for="bookable_order_date">Date</label></th>
-            <td><input name="bookable_order_date" type="text" id="bookable_order_date" class="regular-text pac-target-input" value="<?php echo $date; ?>" required />
+            <th class="sdevs_th" scope="row"><label for="bookable_order_date"><?php echo esc_html__("Date", "sdevs_booking"); ?></label></th>
+            <td><input name="bookable_order_date" type="text" id="bookable_order_date" class="regular-text pac-target-input" value="<?php echo esc_html($date); ?>" required />
                 <p class="description" id="tagline-description">Ex : Sep 14, 2020</p>
             </td>
         </tr>
         <tr>
             <th class="sdevs_th" scope="row"><label for="bookable_order_time">Time</label></th>
-            <td><input name="bookable_order_time" type="time" id="bookable_order_time" value="<?php echo $time; ?>" class="regular-text pac-target-input" required /></td>
+            <td><input name="bookable_order_time" type="time" id="bookable_order_time" value="<?php echo esc_html($time); ?>" class="regular-text pac-target-input" required /></td>
         </tr>
         <tr>
             <th class="sdevs_th" scope="row"><label for="bookable_order_status">Status</label></th>
             <td>
                 <select name="bookable_order_status" id="bookable_order_status">
                     <?php foreach ($statuses as $value => $label) : ?>
-                        <option value="<?php echo $value; ?>" <?php if ($post->post_status == $value) {
-                                                                    echo "selected";
-                                                                }
-                                                                ?>><?php echo $label; ?></option>
+                        <option value="<?php echo esc_html($value); ?>" <?php if ($post->post_status == $value) {
+                                                                            echo "selected";
+                                                                        } ?>><?php echo esc_html($label); ?></option>
                     <?php endforeach; ?>
                 </select>
             </td>
